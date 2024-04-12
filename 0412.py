506d2f8e39ac12b2a1d260e8124b871ff82d698b
BongHee <suksa010@gmail.com>
2024-04-12 10:27:31 +0900

    수정예제

---
+++ 0412.py
@@ -0,0 +1,68 @@
+# num = input("정수 혹은 소수를 입력하세요:")
+#
+# float_num = float(num)
+# print(f'입력한 숫자의 소수형: {float_num}')
+#
+# int_num = int(float(num))
+# print(f'입력한 숫자의 정수형: {int_num}')
+# print(f'int_num의자료형:{type(int_num)}')
+
+# x = '''멀티라인 스트링 닫을떄까지 문자열로 나온다'''
+
+# string = input('영어로 아무거나 입력하세요 (5자 이상):')
+#
+# print(f'str.upper(): {string.upper()}')
+# print(f'str.lower(): {string.lower()}')
+# print(f'str.[2:4]: {string[2:4]}') #[앞,뒤] 앞쪽에 있는건 포함시키고 뒤쪽은 포함시키지 않는다.
+
+
+
+
+#튜플은 소괄호 리스트는 대괄호 (중복 허용 가능)
+#리스트=[]원소 추가 제거
+# l= list()
+#
+# l.append(4)
+# print(l)
+#
+# l.append('String')
+# print(l)
+#
+# l.append('10.44')
+# print(l)
+#
+# l.remove('String')
+# print(l)
+
+
+#set은 순서를 보장하지 않는다.
+# s = set()
+#
+# s.add(10)
+# print(s)
+#
+# s.add('String')
+# print(s)
+#
+# s.add(14)
+# print(s)
+#
+# s.remove('String')
+# print(s)
+
+# d = dict()
+#
+# d['key'] = 'value'
+# print(d)
+#
+# d[4] = 'String'
+# print(d)
+#
+# d[20.2] = 99.4
+# print(d)
+#
+# d.pop(4)
+# print(d)
+
+
+
